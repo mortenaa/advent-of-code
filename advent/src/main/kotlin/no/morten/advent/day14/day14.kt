36548b4539b846176051b7a176fab487cd2b3957
Morten Nygaard Åsnes <morten.aasnes@gmail.com>
2022-12-14 07:57:57 +0100

    Day 14

--- advent/src/main/kotlin/no/morten/advent/day14/day14.kt
+++ advent/src/main/kotlin/no/morten/advent/day14/day14.kt
@@ -5,24 +5,58 @@ import kotlin.math.max
 import kotlin.math.min
 
 fun main() {
-    val inputFile = "day14_test.txt"
-    //val inputFile = "day14.txt"
+    //val inputFile = "day14_test.txt"
+    val inputFile = "day14.txt"
     val input = readResourceFile(inputFile)
-    val (lowPoint, cave) = parse(input)
-    moveSand(lowPoint, cave)
+    var (lowPoint, cave) = parse(input)
+    println(lowPoint)
+    var count = 0
+    do  {
+        var (done, cave) = insertSand2(lowPoint, cave)
+        count+=1
+    } while (500 to 0 !in cave)
+    println(count)
 }
 
-fun moveSand(lowPoint: Int, cave: MutableSet<Pair<Int, Int>>) {
-    val start = 500 to 0
+fun insertSand(lowPoint: Int, cave: MutableSet<Pair<Int, Int>>): Pair<Boolean, MutableSet<Pair<Int, Int>>> {
+    var sand = 500 to 0
     while (true) {
-        var sand = start
+        println(sand)
         if (sand.first to sand.second + 1 !in cave)
             sand = sand.first to sand.second + 1
-        else if (sand.first - 1 to sand.second - 1 !in cave)
-            sand = sand.first - 1 to sand.second - 1
-        else if (sand.first + 1 to sand.second - 1 !in cave)
-            sand = sand.first + 1 to sand.second - 1
-        else 
+        else if (sand.first - 1 to sand.second + 1 !in cave)
+            sand = sand.first - 1 to sand.second + 1
+        else if (sand.first + 1 to sand.second + 1 !in cave)
+            sand = sand.first + 1 to sand.second + 1
+        else {
+            cave.add(sand)
+            return false to cave
+        }
+        if (sand.second > lowPoint)
+            return true to cave
+
+    }
+}
+
+fun insertSand2(lowPoint: Int, cave: MutableSet<Pair<Int, Int>>): Pair<Boolean, MutableSet<Pair<Int, Int>>> {
+    val floor = lowPoint + 2
+    var sand = 500 to 0
+    while (true) {
+        if (sand.first to sand.second + 1 !in cave)
+            sand = sand.first to sand.second + 1
+        else if (sand.first - 1 to sand.second + 1 !in cave)
+            sand = sand.first - 1 to sand.second + 1
+        else if (sand.first + 1 to sand.second + 1 !in cave)
+            sand = sand.first + 1 to sand.second + 1
+        else {
+            cave.add(sand)
+            return false to cave
+        }
+        if (sand.second + 1 == floor) {
+            cave.add(sand)
+            return false to cave
+        }
+
     }
 }
 
@@ -34,9 +68,9 @@ fun parse(input: String): Pair<Int, MutableSet<Pair<Int, Int>>> {
             it.substringBefore(",").toInt() to it.substringAfter(",").toInt()
         }.zipWithNext { a, b ->
             println("$a->$b")
-            if (a.second < lowPoint)
+            if (a.second > lowPoint)
                 lowPoint = a.second
-            if (b.second < lowPoint)
+            if (b.second > lowPoint)
                 lowPoint = b.second
             if (a.first == b.first) {
                 for (i in min(a.second, b.second) .. max(a.second, b.second))
